088377090c971ff918a8a8aaf7364a6e2285a976
dniel <daniel@engfeldt.net>
2020-08-30 19:05:23 +0200

    refactor certificates to use new kubernetes-alpha provider

--- modules/certificates/main.tf
+++ modules/certificates/main.tf
@@ -5,30 +5,69 @@ locals {
   })
 }
 
-resource "k8s_manifest" "certificate" {
+resource "kubernetes_manifest" "certificate" {
   for_each = var.certificates
-
-  content = templatefile("${path.module}/templates/certificate.tpl", {
-    app_name    = local.app_name,
-    domain_name = var.domain_name,
-    name_prefix = var.name_prefix,
-    name        = each.key
-    secretName  = each.value.secretName,
-    dnsName     = each.value.dnsName
-  })
-  namespace = each.value.namespace
+  provider = kubernetes-alpha
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1alpha3"
+    "kind" = "Certificate"
+    "metadata" = {
+      "labels" = local.labels
+      "name" = each.key
+      "namespace" = each.value.namespace
+    }
+    "spec" = {
+      "commonName" = each.value.dnsName
+      "dnsNames" = [
+        each.value.dnsName
+      ]
+      "issuerRef" = {
+        "kind" = "ClusterIssuer"
+        "name" = "letsencrypt-${var.name_prefix}-issuer"
+      }
+      "renewBefore" = "360h"
+      "secretName" = each.value.secretName
+    }
+  }
 }
 
-
-resource "k8s_manifest" "cluster_issuer" {
-  namespace = "cert-manager"
-  content = templatefile("${path.module}/templates/clusterissuer.tpl", {
-    app_name       = local.app_name,
-    domain_name    = var.domain_name,
-    name_prefix    = var.name_prefix,
-    hosted_zone_id = var.hosted_zone_id
-    accesskey      = var.aws_access_key
-  })
+resource "kubernetes_manifest" "cluster_issuer" {
+  provider = kubernetes-alpha
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1alpha3"
+    "kind" = "ClusterIssuer"
+    "metadata" = {
+      "name" = "letsencrypt-${var.name_prefix}-issuer"
+    }
+    "spec" = {
+      "acme" = {
+        "email" = "daniel@engfeldt.net"
+        "privateKeySecretRef" = {
+          "name" = "letsencrypt-dns01-prod"
+        }
+        "server" = "https://acme-v02.api.letsencrypt.org/directory"
+        "solvers" = [
+          {
+            "dns01" = {
+              "route53" = {
+                "accessKeyID" = var.aws_access_key
+                "hostedZoneID" = var.hosted_zone_id
+                "region" = "eu-central-1"
+                "role" = ""
+                "secretAccessKeySecretRef" = {
+                  "key" = "AWS_SECRET_KEY"
+                  "name" = "${var.name_prefix}-route53-creds"
+                }
+              }
+            }
+            "selector" = {
+              "dnsZones" = [var.domain_name]
+            }
+          },
+        ]
+      }
+    }
+  }
 }
 
 resource "kubernetes_secret" "route53-credentials" {
