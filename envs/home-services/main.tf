353f0e5ae4ad8aab8e1686e9d6e727ae6a38b12c
dniel <daniel@engfeldt.net>
2020-10-18 19:24:31 +0200

    remove gitsecret encrypted variables and use AWS Secret Manager instead for secrets for home-services.

--- envs/home-services/main.tf
+++ envs/home-services/main.tf
@@ -1,27 +1,59 @@
+# used to give traefik access to route53 to do ACME letencrypt certificates.
+data "aws_secretsmanager_secret_version" "traefik" {
+  secret_id = "traefik"
+}
+
+# auth0 credentials
+data "aws_secretsmanager_secret_version" "auth0" {
+  secret_id = "auth0"
+}
+locals {
+  auth0_domain                       = "dniel.eu.auth0.com"
+  auth0_client_id                    = jsondecode(data.aws_secretsmanager_secret_version.auth0.secret_string)["client_id"]
+  auth0_client_secret                = jsondecode(data.aws_secretsmanager_secret_version.auth0.secret_string)["client_secret"]
+  kube_context                       = "juju-context"
+  kube_config                        = "~/.kube/config"
+  aws_region                         = "eu-north-1"
+  name_prefix                        = "services"
+  base_domain_name                   = "dniel.in"
+  load_balancer_public_ip            = "10.0.50.165"
+  load_balancer_alias_hosted_zone_id = ""
+  load_balancer_alias_dns_name       = ""
+  primary_hosted_zone_id             = "Z25Z86AZE76SY4"
+  traefik_websecure_port             = 32443
+  traefik_service_type               = "NodePort"
+  traefik_default_tls_secretName     = "traefik-default-tls"
+  traefik_helm_chart_version         = "9.3.0"
+  traefik_aws_access_key             = jsondecode(data.aws_secretsmanager_secret_version.traefik.secret_string)["access_key"]
+  traefik_aws_secret_key             = jsondecode(data.aws_secretsmanager_secret_version.traefik.secret_string)["secret_key"]
+  traefik_pilot_token                = "685b7f76-2f1e-4f30-8b00-8e54e58ce6a8"
+  forwardauth_helm_chart_version     = "2.0.8"
+}
 #########################################
 #
 #
 #########################################
 provider "auth0" {
-  domain        = var.auth0_domain
-  client_id     = var.auth0_client_id
-  client_secret = var.auth0_client_secret
+  domain        = local.auth0_domain
+  client_id     = local.auth0_client_id
+  client_secret = local.auth0_client_secret
 }
 provider "kubernetes" {
-  config_context = var.kube_context
+  config_context = local.kube_context
 }
 provider "kubernetes-alpha" {
-  config_context = var.kube_context
-  config_path    = var.kube_config
+  config_context = local.kube_context
+  config_path    = local.kube_config
 }
 provider "helm" {
   kubernetes {
-    config_context = var.kube_context
+    config_context = local.kube_context
   }
 }
 provider "aws" {
-  region = var.aws_region
+  region = local.aws_region
 }
+
 
 #########################################
 #
@@ -29,27 +61,27 @@ provider "aws" {
 #########################################
 module "template" {
   source           = "../../modules/template"
-  base_domain_name = var.base_domain_name
-  name_prefix      = var.name_prefix
+  base_domain_name = local.base_domain_name
+  name_prefix      = local.name_prefix
 
   # parameters for traefik
-  traefik_websecure_port         = var.traefik_websecure_port
-  traefik_service_type           = var.traefik_service_type
-  traefik_default_tls_secretName = var.traefik_default_tls_secretName
-  traefik_pilot_token            = var.traefik_pilot_token
-  traefik_aws_access_key         = var.traefik_aws_access_key
-  traefik_aws_secret_key         = var.traefik_aws_secret_key
-  traefik_helm_chart_version     = var.traefik_helm_chart_version
+  traefik_websecure_port         = local.traefik_websecure_port
+  traefik_service_type           = local.traefik_service_type
+  traefik_default_tls_secretName = local.traefik_default_tls_secretName
+  traefik_pilot_token            = local.traefik_pilot_token
+  traefik_aws_access_key         = local.traefik_aws_access_key
+  traefik_aws_secret_key         = local.traefik_aws_secret_key
+  traefik_helm_chart_version     = local.traefik_helm_chart_version
 
   # DNS configuration and load balancer parameters.
-  load_balancer_public_ip            = var.load_balancer_public_ip
-  load_balancer_alias_dns_name       = var.load_balancer_alias_dns_name
-  load_balancer_alias_hosted_zone_id = var.load_balancer_alias_hosted_zone_id
-  primary_hosted_zone_id             = var.primary_hosted_zone_id
+  load_balancer_public_ip            = local.load_balancer_public_ip
+  load_balancer_alias_dns_name       = local.load_balancer_alias_dns_name
+  load_balancer_alias_hosted_zone_id = local.load_balancer_alias_hosted_zone_id
+  primary_hosted_zone_id             = local.primary_hosted_zone_id
 
   # Forwardauth parameters
-  forwardauth_helm_chart_version = var.forwardauth_helm_chart_version
-  forwardauth_auth0_domain       = var.auth0_domain
+  forwardauth_helm_chart_version = local.forwardauth_helm_chart_version
+  forwardauth_auth0_domain       = local.auth0_domain
 
 }
 
@@ -59,8 +91,8 @@ module "template" {
 #########################################
 module "template-services" {
   source           = "../../modules/template-services"
-  base_domain_name = var.base_domain_name
-  name_prefix      = var.name_prefix
+  base_domain_name = local.base_domain_name
+  name_prefix      = local.name_prefix
   hosted_zone_id   = module.template.hosted_zone_id
 
   feature_monitoring = true
