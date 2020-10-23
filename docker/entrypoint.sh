1c6da586ea032c8537d708b6c2ecf3289130e5ac
dniel <daniel@engfeldt.net>
2020-10-23 19:21:04 +0200

    write shell script for entrypoint

---
+++ docker/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+################################################################################
+# Help                                                                         #
+################################################################################
+Help() {
+  # Display Help
+  echo "Run Terraform apply."
+  echo "Syntax: entrypoint.sh <environment>"
+  echo
+}
+
+################################################################################
+# Retrieve Kubeconfig from AWS Secretsmanager
+################################################################################
+Kubeconf(){
+  echo "Get kubeconf.."
+  mkdir ~/.kube;
+  aws secretsmanager get-secret-value --secret-id kubeconfig | jq --raw-output '.SecretString' > ~/.kube/config;
+}
+
+################################################################################
+# Apply                                                                         #
+################################################################################
+Apply() {
+  echo "Apply Terraform.."
+  cd envs/$1 || exit
+  terraform init -input=false
+  terraform apply -auto-approve
+}
+
+if [ "$1" != "" ]; then
+  Kubeconf
+  Apply
+else
+  Help
+fi
+
