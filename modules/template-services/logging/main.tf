1f56dec9349d6f142f9528e9ff2a8aaf537d8233
dniel <daniel@engfeldt.net>
2021-01-30 15:31:32 +0100

    Install Elasticsearch via operator

--- modules/template-services/logging/main.tf
+++ modules/template-services/logging/main.tf
@@ -11,31 +11,31 @@ locals {
 # Deploy Elasticsearch
 #
 ######################################################
-//resource "kubernetes_manifest" "elasticsearch" {
-//  provider   = kubernetes-alpha
-//
-//  manifest = {
-//    "apiVersion" = "elasticsearch.k8s.elastic.co/v1"
-//    "kind"       = "Elasticsearch"
-//    "metadata" = {
-//      "namespace" = var.name_prefix
-//      "labels"    = local.labels
-//      "name"      = "elastic"
-//    }
-//    "spec" = {
-//      "version": "7.10.2"
-//      "nodeSets" = [
-//        {
-//          "name" = "default"
-//          "count" = 1
-//          "config" = {
-//            "node.store.allow_mmap" = false
-//            "node.master" = true
-//            "node.data" = true
-//            "node.ingres" = true
-//          }
-//        }
-//      ]
-//    }
-//  }
-//}
+resource "kubernetes_manifest" "elasticsearch" {
+  provider   = kubernetes-alpha
+
+  manifest = {
+    "apiVersion" = "elasticsearch.k8s.elastic.co/v1"
+    "kind"       = "Elasticsearch"
+    "metadata" = {
+      "namespace" = var.name_prefix
+      "labels"    = local.labels
+      "name"      = "elastic"
+    }
+    "spec" = {
+      "version": "7.10.2"
+      "nodeSets" = [
+        {
+          "name" = "default"
+          "count" = 1
+          "config" = {
+            "node.store.allow_mmap" = false
+            "node.master" = true
+            "node.data" = true
+            "node.ingres" = true
+          }
+        }
+      ]
+    }
+  }
+}
