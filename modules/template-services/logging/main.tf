286bd2142a062e0ed0a5e1d6681513c4207452a6
dniel <daniel@engfeldt.net>
2021-01-31 09:48:23 +0100

    Add docu

--- modules/template-services/logging/main.tf
+++ modules/template-services/logging/main.tf
@@ -1,6 +1,14 @@
 ######################################################
 # Install ElasticCloud on K8s for centralized logging.
+# https://github.com/elastic/cloud-on-k8s
 #
+# This module is a little bit special beacuse its the
+# only module that use banzaicloud/k8s to apply
+# manifests instead of kubernetes-alpha from hashicorp.
+#
+# The reason for that is the alpha provider didnt work
+# well with the provider to create new resources and
+# using the terraform plan with dry-run.
 ######################################################
 locals {
   labels = merge(var.labels, {
