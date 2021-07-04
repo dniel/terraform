5cc7bc0aa5feb38fc178876d18d5d5cd59a42855
dniel <daniel@engfeldt.net>
2021-07-04 15:16:00 +0200

    update docu.

--- modules/template-services/logging/main.tf
+++ modules/template-services/logging/main.tf
@@ -1,14 +1,7 @@
 ######################################################
-# Install ElasticCloud on K8s for centralized logging.
-# https://github.com/elastic/cloud-on-k8s
+# Install Grafana Loki  for centralizeed log indexing.
+# See https://grafana.com/oss/loki/
 #
-# This module is a little bit special beacuse its the
-# only module that use banzaicloud/k8s to apply
-# manifests instead of kubernetes-alpha from hashicorp.
-#
-# The reason for that is the alpha provider didnt work
-# well with the provider to create new resources and
-# using the terraform plan with dry-run.
 ######################################################
 locals {
   labels = merge(var.labels, {
