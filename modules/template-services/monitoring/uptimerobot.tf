d07d6afefcbccca72578094518920df10913a42b
dniel <daniel@engfeldt.net>
2021-07-04 13:47:52 +0200

    add uptime robot exporter for prometheus.

---
+++ modules/template-services/monitoring/uptimerobot.tf
@@ -0,0 +1,19 @@
+# auth0 credentials
+data "aws_secretsmanager_secret_version" "uptimerobot" {
+  secret_id = "uptimerobot"
+}
+
+# Prometheus Exporter for the official uptimerobot CLI
+resource "helm_release" "uptimerobot-prometheus" {
+  name       = "uptimerobot-prometheus"
+  repository = "https://k8s-at-home.com/charts/"
+  chart      = "uptimerobot-prometheus"
+  namespace  = var.name_prefix
+  version    = "3.0.0"
+
+  # API key used for uptimerobot stored in AWS Secrets Manager.
+  set {
+    name  = "env.UPTIMEROBOT_API_KEY"
+    value = data.aws_secretsmanager_secret_version.uptimerobot.secret_string
+  }
+}
